8cb8a7bc0151416d2d1c2099cc0ab82f20dc7348
JuanVergara-9 <juanvergara920@gmail.com>
2024-12-10 20:55:10 -0300

    docker

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345678@localhost/orquestador'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:12345678@localhost:5432/orquestador')
     CACHE_TYPE = 'simple'  # Configuración básica de caché
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CATALOG_SERVICE_URL = 'http://localhost:5000/inventario'
-    PAYMENT_SERVICE_URL = 'http://localhost:5001/catalogo'
-    INVENTORY_SERVICE_URL = 'http://localhost:5002/compras'
-    PURCHASE_SERVICE_URL = 'http://localhost:5003/pagos'
+    
+    INVENTORY_SERVICE_URL = 'http://localhost:5000/inventario'
+    CATALOG_SERVICE_URL = 'http://localhost:5001/catalogo'
+    PURCHASE_SERVICE_URL = 'http://localhost:5002/compras'
+    PAYMENT_SERVICE_URL = 'http://localhost:5003/pagos'
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from app import create_app
+from app.extension import db
+from flask_migrate import Migrate
 
 app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5003)
+    app.run(port=5005)
