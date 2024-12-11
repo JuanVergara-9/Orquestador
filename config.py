54614a99a05f3af50882af0d4e2be716d37ca01a
JuanVergara-9 <juanvergara920@gmail.com>
2024-12-10 22:33:01 -0300

    config

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:12345678@localhost:5432/orquestador')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:12345678@db/orquestador')
     CACHE_TYPE = 'simple'  # Configuración básica de caché
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    INVENTORY_SERVICE_URL = 'http://localhost:5000/inventario'
-    CATALOG_SERVICE_URL = 'http://localhost:5001/catalogo'
-    PURCHASE_SERVICE_URL = 'http://localhost:5002/compras'
-    PAYMENT_SERVICE_URL = 'http://localhost:5003/pagos'
+    INVENTORY_SERVICE_URL = os.getenv('INVENTORY_SERVICE_URL', 'http://ms_inventario:5000/inventario')
+    CATALOG_SERVICE_URL = os.getenv('CATALOG_SERVICE_URL', 'http://ms_catalogo:5001/catalogo')
+    PURCHASE_SERVICE_URL = os.getenv('PURCHASE_SERVICE_URL', 'http://ms_compras:5002/compras')
+    PAYMENT_SERVICE_URL = os.getenv('PAYMENT_SERVICE_URL', 'http://ms_pagos:5003/pagos')
