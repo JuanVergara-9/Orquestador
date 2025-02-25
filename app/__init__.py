806f8290ce9b4c2945b5a9033b7fed33aa4a4efb
JuanVergara-9 <juanvergara920@gmail.com>
2025-02-25 14:35:19 -0300

    limit

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from app.extension import db, cache, migrate
 from app.routes import orchestrator_bp
 from config import Config
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+# Inicializar Limiter
+limiter = Limiter(key_func=get_remote_address)
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +18,7 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     cache.init_app(app)
+    limiter.init_app(app)  # Inicializar Limiter con la aplicación
 
     app.register_blueprint(orchestrator_bp, url_prefix='/orchestrator')
 
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, jsonify
 from .services import process_order
+from . import limiter
 
 orchestrator_bp = Blueprint('orchestrator', __name__)
 
 @orchestrator_bp.route('/order', methods=['POST'])
+@limiter.limit("5 por minuto")  # Limita a cinco solicitudes por minuto
 def order():
     order_data = request.json
     result = process_order(order_data)
